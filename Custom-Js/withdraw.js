d27e59129a8901a5c8a7d3fbfa47b0d29222db6c
JuwelShaikh <juwelshaikh04@gmail.com>
2023-09-08 15:47:05 +0600

    Withdraw and balance update

--- Custom-Js/withdraw.js
+++ Custom-Js/withdraw.js
@@ -1,3 +1,25 @@
 document.getElementById('btn-withdraw').addEventListener('click',function(){
-    console.log('clicked');
+    const withdrawInput = document.getElementById('withdraw-input');
+    const newWithdrawString = withdrawInput.value;
+    const newWithdraw = parseFloat(newWithdrawString);
+    
+    
+
+    const prevWithdrawField = document.getElementById('withdraw-feild');
+    const prevWithdrawString = prevWithdrawField.innerText;
+    const prevWithdraw = parseFloat(prevWithdrawString);
+
+    const totalWithdraw = prevWithdraw + newWithdraw;
+    
+    prevWithdrawField.innerText = totalWithdraw;
+
+    // balance
+    const balanceField = document.getElementById('balance-Field');
+    const prevBalString = balanceField.innerText;
+    const prevBal = parseFloat(prevBalString);
+    
+    const totalBal = prevBal - newWithdraw;
+    
+    balanceField.innerText = totalBal;
+    withdrawInput.value = ''; 
 })
